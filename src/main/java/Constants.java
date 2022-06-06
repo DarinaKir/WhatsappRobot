cc86a8115211289082d2a2477392c4e79591cf8c
DarinaKir <dasha23kir@gmail.com>
2022-06-06 14:10:19 +0300

    06.06.2022

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,5 +1,5 @@
 public class Constants {
-    public static final String webWhatsappAddress = "https://web.whatsapp.com/";
+    public static final String WEB_WHATSAPP_ADDRESS = "https://web.whatsapp.com/";
 
     public static final int WINDOW_WIDTH = 900;
     public static final int WINDOW_HEIGHT = 550;
@@ -7,4 +7,7 @@ public class Constants {
     public static final int BUTTON_WIDTH = 170;
     public static final int BUTTON_HEIGHT = 60;
     public static final int BUTTON_FONT_SIZE = 20;
+
+    public static final int CONNECTED_TEXT_WIDTH = 220;
+
 }
--- src/main/java/MainPanel.java
+++ src/main/java/MainPanel.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MainPanel extends JPanel {
@@ -8,6 +9,9 @@ public class MainPanel extends JPanel {
     private JButton whatsappButton;
 
     private Font buttonFont;
+    private  Font textFont;
+
+    private boolean isConnected;
 
 //    private ChromeDriver driver;
 
@@ -16,46 +20,43 @@ public class MainPanel extends JPanel {
         this.setBounds(x, y , width, height);
 
         this.buttonFont = new Font("David",Font.BOLD,Constants.BUTTON_FONT_SIZE);
+        this.textFont = new Font("David",Font.ROMAN_BASELINE, Constants.BUTTON_FONT_SIZE);
+
         this.whatsappButton = new JButton();
         this.whatsappButton.setBounds((Constants.WINDOW_WIDTH - Constants.BUTTON_WIDTH)/2, Constants.WINDOW_HEIGHT/2, Constants.BUTTON_WIDTH, Constants.BUTTON_HEIGHT);
         this.whatsappButton.setText("Web Whatsapp");
         this.whatsappButton.setFont(this.buttonFont);
         this.add(this.whatsappButton);
+        this.isConnected = false;
+
+//        JLabel connectedText = new JLabel("You are not connected yet...");
+        JLabel connectedText = new JLabel();
+        connectedText.setBounds((this.whatsappButton.getX()-(Constants.CONNECTED_TEXT_WIDTH - this.whatsappButton.getWidth())/2),
+                this.whatsappButton.getY() + this.whatsappButton.getHeight(), Constants.CONNECTED_TEXT_WIDTH, this.whatsappButton.getHeight());
+        connectedText.setFont(this.textFont);
+        this.add(connectedText);
+
         System.setProperty("webdriver.chrome.driver",
                 "C:\\Users\\dasha\\Downloads\\chromedriver_win32\\chromedriver.exe");
-
         this.whatsappButton.addActionListener((e) -> {
             ChromeDriver driver = new ChromeDriver();
-            driver.get(Constants.webWhatsappAddress);
-
-            boolean check = false;
-            while (!check){
+            driver.get(Constants.WEB_WHATSAPP_ADDRESS);
+            while (!this.isConnected){
                 try {
-                    check = driver.findElement(By.id("side")) != null;
-                }catch (Exception exception){
+                    this.isConnected = (driver.findElement(By.id("side")).isDisplayed());
+                }catch (NoSuchElementException exception){
                 }
             }
+            connectedText.setText("You were able to connect!");
         });
 
-//        this.whatsappButton.addActionListener((e) -> {
-//            this.driver = new ChromeDriver();
-//            this.driver.get(Constants.webWhatsappAddress);
-//            driver.manage().window().maximize();
-//            new Thread(() -> {
-//                System.out.println("in thread");
-//                while (true){
-//                    if(driver.findElement(By.id("side")).isDisplayed()){
-//                        System.out.println("is in");
-//                    }
-//                    try {
-//                        Thread.sleep(1);
-//                    } catch (InterruptedException exception) {
-//                        exception.printStackTrace();
-//                    }
-//                }
-//
-//            }).start();
-//        });
+//        new Thread(() -> {
+//            try {
+//                Thread.sleep(1);
+//            } catch (InterruptedException exception) {
+//                exception.printStackTrace();
+//            }
+//        }).start();
 
     }
 }
